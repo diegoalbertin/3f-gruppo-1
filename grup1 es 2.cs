ef4cfa6e1e70ef95a2ca083ed9868eea9e85f833
diegoalbertin <72445590+diegoalbertin@users.noreply.github.com>
2020-11-13 08:05:11 +0100

    Create grup1 es 2.cs

---
+++ grup1 es 2.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+namespace Esercizio2_Gruppo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci i numeri che vuoi classificare");
+            double x;
+            x = Convert.ToDouble(Console.ReadLine());
+            double y;
+            y = Convert.ToDouble(Console.ReadLine());
+            ClassificazioneNumeri(x, y);
+            Console.ReadKey();
+        }
+        static double ClassificazioneNumeri(double x, double y)
+        {
+
+            for (; ; )//for infinito
+            {
+                if (x > y)
+                {
+                    Console.WriteLine("{0} > {1}, inserisci un'altro numero per confrontarlo con {2}", x, y, x);
+                    y = Convert.ToDouble(Console.ReadLine());
+                }
+                else
+                {
+                    if (x < y)
+                    {
+                        Console.WriteLine("{0} > {1}, inserisci un'altro numero per confrontarlo con {2}", y, x, y);
+                        x = Convert.ToDouble(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} = {1}, inserisci un'altro numero per confrontarlo con {2}", y, x, y);
+                        x = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+            }
+        }
+    }
+}
+
